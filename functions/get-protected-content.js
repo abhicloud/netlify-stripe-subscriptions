ed89e3f5c7a6fdfb1d6d3fc3556ef1424f778c24
abhicloud <63748892+abhicloud@users.noreply.github.com>
2021-01-06 17:20:24 -0800

    Update get-protected-content.js

--- functions/get-protected-content.js
+++ functions/get-protected-content.js
@@ -20,7 +20,7 @@ const content = {
   },
   premium: {
     src:
-      'https://images.unsplash.com/photo-1519098901909-b1553a1190af?auto=format&fit=crop&w=600&h=600&q=80',
+      'https://images.unsplash.com/photo-1546975490-e8b92a360b24?auto=format&fit=crop&w=600&h=600&q=80',
     alt: 'corgi in a tent with string lights in the foreground',
     credit: 'Cole Keister',
     creditLink: 'https://unsplash.com/photos/cX-KEISwDIw',
